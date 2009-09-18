eb4be6faec46b5d131943e6715c866f61cd4b9e0
sfanimorphic <sfanimorphic@284f36b8-9d99-11de-a36b-d11e950a0b04>
2009-09-18 20:57:27 +0000

    Added: > Trace class, for printing debug statements > Packet class, for handling serialization tasks > Opcode enumeration, for denoting the purpose of packet instances > ServerSocketThread, for managing a blocking socket listener > PacketQueueThread, for maintaining contact with a client socket
    Modified:
    > EuchreServer.java
     > Added initial socket creation routine

--- trunk/src/server/EuchreServer.java
+++ trunk/src/server/EuchreServer.java
@@ -1,12 +1,49 @@
 package server;
 
-public class EuchreServer {
+import java.io.*;
+import java.net.*;
+import utility.*;
+
+public class EuchreServer
+{
 
 	/**
-	 * @param args
+	 * @param args argument 1 is a port number to listen on. it is optional; port 36212 will be used if it is omitted.
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)
+	{
+		// the default listen port is 36212
+		int port = 36212;
+		ServerSocket serverSocket = null;
+
+		// the first argument is the port (the zeroith is the program name)
+		if (args.length >= 2)
+			port = Integer.parseInt(args[1]);
+
+		// make sure the port is valid
+		if (port < 100 || port > 65534)
+			System.out.println(port + " is not a valid port to listen on. Please enter a number between 100 and 65534.");
+
+		try
+		{
+			// the ip address '0.0.0.0' refers to a universal bind point. any address will be able to connect to it.
+			// the 4 refers to the socket backlog, which is how many connections it will allow at any given time
+			serverSocket = new ServerSocket(port, 4, Inet4Address.getByAddress(new byte[] { 0, 0, 0, 0 }));
+		} catch (IOException e)
+		{
+			Trace.dprint("Could not listen on port: %d", port);
+			Trace.dprint("Exception message: %s", e.getMessage());
+			System.exit(-1);
+		}
+
+		try
+		{
+			// close the socket
+			serverSocket.close();
+		} catch (IOException e)
+		{
+			Trace.dprint("Could not close socket. Message: %s", e.getMessage());
+		}
 
 	}
 
---
+++ trunk/src/server/ServerSocketThread.java
@@ -0,0 +1,24 @@
+package server;
+
+import java.net.*;
+
+/**
+ * Represents a thread in which a socket will block while waiting for incoming
+ * connections.
+ * 
+ */
+public class ServerSocketThread extends Thread
+{
+	private Socket socket = null;
+
+	public ServerSocketThread(Socket socket)
+	{
+		super("ServerSocketThread");
+		this.socket = socket;
+	}
+
+	public void run()
+	{
+		
+	}
+}
