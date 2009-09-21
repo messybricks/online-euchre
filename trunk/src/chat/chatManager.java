304cf7d79283e1167efbaa0753727942999d8c36
KingJason1 <KingJason1@284f36b8-9d99-11de-a36b-d11e950a0b04>
2009-09-21 18:43:58 +0000

    Started adding chat manager stuff

--- trunk/src/chat/chatManager.java
+++ trunk/src/chat/chatManager.java
@@ -11,7 +11,7 @@ class chatManager
     public boolean send(chatObject obj)
     {
         User dest = obj.getDest();
-      //  Packet pckt = new Packet(opcode, obj);
+      //Packet pckt = new Packet(opcode, obj);
         return false;
     }
 }
