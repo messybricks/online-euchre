b115710b0dbe4ab4e345acf6b60f88e9d41d0c2c
KingJason1 <KingJason1@284f36b8-9d99-11de-a36b-d11e950a0b04>
2009-09-21 14:40:15 +0000

    Added chat files

---
+++ trunk/src/chat/User.java
@@ -0,0 +1,15 @@
+class User implements Serializable
+{
+    private String username;
+
+    public User(String user)
+    {
+        username = user;
+    }
+
+    public String getUsername
+    {
+        return username;
+    }
+
+}
---
+++ trunk/src/chat/chatManager.java
@@ -0,0 +1,13 @@
+class chatManager
+{
+    public chatManager()
+    {
+    }
+
+    public boolean send(chatObject obj)
+    {
+        User dest = obj.getDest();
+        Packet pckt = new Packet(opcode, obj);
+
+    }
+}
