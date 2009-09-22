bf7e36691a3425726c2e24413e70438f682b6f33
messybricks <messybricks@284f36b8-9d99-11de-a36b-d11e950a0b04>
2009-09-22 19:41:54 +0000

    capitalized class name

---
+++ trunk/src/chat/ChatObject.java
@@ -0,0 +1,43 @@
+package chat;
+
+class ChatObject
+{
+    private User sourceUser;
+    private User destUser;
+    private String message;
+
+    /**
+     * Creates a chat object to transmit a String from one
+     * user to another.
+     *
+     * @param source the user that sent the message
+     * @param dest the user to recieve the message
+     * @param mess the String to be sent
+     */
+    public ChatObject(User source, User dest, String mess)
+    {
+        sourceUser = source;
+	destUser = dest;
+	message = mess;
+    }
+
+    /**
+     * Get the user that sent the message
+     *
+     * @returns User the user that sent the message
+     */
+    public User getSource()
+    {
+        return sourceUser;
+    }
+
+    /**
+     * Get the user that recieves the message
+     *
+     * @returns User the User that recieves the message
+     */
+    public User getDest()
+    {
+        return destUser;
+    }
+}
