f52452a656e6e64ea26d88b0ae5d83388f658100
sfAnimorphic <sfAnimorphic@284f36b8-9d99-11de-a36b-d11e950a0b04>
2009-09-22 20:51:47 +0000

    Added:  > InvalidOpcodeException  > InvalidPacketException  > fromOrdinal method in Opcode enumeration  > Packet's expanding reconstructor  > dprintArray method to Trace Fixed:  > Packet.flatten() bug where the header written used one-byte integers instead of four

--- src/utility/Opcode.java
+++ src/utility/Opcode.java
@@ -2,10 +2,26 @@ package utility;
 
 public enum Opcode
 {
+	// server/client connection commands
 	Ping,
 	Pong,
 	Quit,
 
-	//Chat commands
-	SendMessage,
+	// chat commands
+	SendMessage;
+
+	/**
+	 * Generates an instance of Opcode with the given ordinal value.
+	 * @param ordinal Ordinal to use
+	 * @return Opcode with given ordinal
+	 * @throws InvalidOpcodeException if the given ordinal does not exist in the Opcode enumeration
+	 */
+	public static Opcode fromOrdinal(int ordinal) throws InvalidOpcodeException
+	{
+		for(Opcode value : Opcode.values())
+			if(value.ordinal() == ordinal)
+				return value;
+		
+		throw new InvalidOpcodeException(ordinal);
+	}
 }
--- src/utility/Packet.java
+++ src/utility/Packet.java
@@ -2,26 +2,92 @@ package utility;
 
 import java.io.*;
 
+/**
+ * A packet is a datum to be sent over a network interface. This implementation is expanded to include automatic data
+ * serialization, so it becomes possible to seemingly send objects directly over the internet. A packet consists of an
+ * A packet contains an opcode, which defines why the packet is sent, and a datum which describes the operation.
+ */
 public class Packet
 {
 	private Opcode opcode;
 	private Serializable data;
+	
+	private static final int HEADER_SIZE = 8;
 
+	/**
+	 * Creates a new instance of the Packet class.
+	 * @param opcode Opcode defining the action to perform
+	 */
 	public Packet(Opcode opcode)
 	{
 		this(opcode, null);
 	}
 
+	/**
+	 * Creates a new instance of the Packet class.
+	 * @param opcode Opcode defining the action to perform
+	 * @para data Some serializable object describing how to perform the action
+	 */
 	public Packet(Opcode opcode, Serializable data)
 	{
 		this.opcode = opcode;
 		this.data = data;
 	}
+	
+	/**
+	 * Recreates a packet from a byte array created using the Packet.flatten() method.
+	 * @param array A byte array created using the Packet.flatten() method
+	 * @throws InvalidPacketException if the given array does not represent a valid flattened Packet object
+	 * @throws IllegalArgumentException if array is null
+	 */
+	public Packet(byte[] array) throws InvalidPacketException, IllegalArgumentException
+	{
+		// check for validity
+		if(array == null)
+			throw new IllegalArgumentException("Cannot instantiate a packet from a null array.");
+		if(array.length < HEADER_SIZE)
+			throw new InvalidPacketException("Packet contains a corrupted header.");
+		
+		ByteArrayInputStream stream = new ByteArrayInputStream(array);
+		DataInputStream dataIn = new DataInputStream(stream);
+		
+		try
+		{
+			// decode the opcode from its ordinal
+			opcode = Opcode.fromOrdinal(dataIn.readInt());
+			int dataSize = dataIn.readInt();
+			
+			// if dataSize is zero, then we have no datum to read. otherwise, deserialize it
+			if(dataSize == 0)
+				data = null;
+			else
+			{
+				ObjectInputStream objectIn = new ObjectInputStream(stream);
+				// the following cast cannot fail, because if an object is not serializable, readObject() will fail
+				data = (Serializable) objectIn.readObject();
+			}
+		}
+		catch (InvalidOpcodeException ex)
+		{
+			throw new InvalidPacketException(ex.getMessage());
+		}
+		catch (ClassNotFoundException ex)
+		{
+			throw new InvalidPacketException("Packet referenced a typename that does not exist in this AppDomain.");
+		}
+		catch (IOException ex)
+		{
+			throw new InvalidPacketException(ex.getMessage());
+		}
+	}
 
 	// the structure of a flattened packet follows this pattern
 	// 4 byte integer opcode
 	// 4 byte integer packet size
-	// variable size reflexive packet
+	// variable size packet datum
+	/**
+	 * Flattens this packet into an array of bytes to be sent over a network.
+	 */
 	public byte[] flatten()
 	{
 		ByteArrayOutputStream stream = null;
@@ -32,7 +98,7 @@ public class Packet
 			if(data == null)
 			{
 				// if data is null, we create a simple packet of 8 bytes. it contains the opcode, and signifies that there is no data by writing a size of 0.
-				stream = new ByteArrayOutputStream(8);
+				stream = new ByteArrayOutputStream(HEADER_SIZE);
 				bitConverter = new DataOutputStream(stream);
 				bitConverter.write(opcode.ordinal());
 				bitConverter.write(0);
@@ -48,9 +114,9 @@ public class Packet
 				// next, we create our output stream, write the opcode and packet size, and then flush the packet data to the output stream.
 				stream = new ByteArrayOutputStream();
 				bitConverter = new DataOutputStream(stream);
-				bitConverter.write(opcode.ordinal());
-				bitConverter.write(target.size());
-				target.writeTo(stream);
+				bitConverter.writeInt(opcode.ordinal());
+				bitConverter.writeInt(target.size());
+				stream.write(target.toByteArray());
 			}
 			
 			return stream.toByteArray();
