64a8a67b0fb399322fd5b6bd60b21a57dcac80c3
sfAnimorphic <sfAnimorphic@284f36b8-9d99-11de-a36b-d11e950a0b04>
2009-09-23 19:38:11 +0000

    Added:  > EuchreNetClient, which manages a connection to the EuchreServer  > Functions to create and destroy the EuchreApplet, and to attach it to a server  > Implementation for ServerSocketThread  > Management loop on EuchreServer's main thread with exit command Notes:  > The client and server can actually connect to each other now, yay!

--- src/client/EuchreApplet.java
+++ src/client/EuchreApplet.java
@@ -2,8 +2,26 @@ package client;
 
 import java.applet.*;
 
-public class EuchreApplet extends Applet {
+import javax.swing.JOptionPane;
 
+public class EuchreApplet extends Applet
+{
+	private EuchreNetClient client = null;
+	
 	private static final long serialVersionUID = 1L;
+	
+	public void init()
+	{
+		client = new EuchreNetClient("127.0.0.1", 36212);
+		if(!client.isValid())
+		{
+			JOptionPane.showMessageDialog(this, "Unable to establish connection with server. Cannot continue.");
+			System.exit(-1);
+		}
+	}
 
+	public void destroy()
+	{
+		client.dispose();
+	}
 }
--- src/server/PacketQueueThread.java
+++ src/server/PacketQueueThread.java
@@ -1,17 +1,98 @@
 package server;
 
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import utility.*;
+
 /**
  * This thread manages two queues; one for sending data over the network and one for receiving it.
  */
 public class PacketQueueThread extends Thread
 {
-	public PacketQueueThread()
+	private Socket socket = null;
+	private Queue<Packet> inbound = null;
+	private Queue<Packet> outbound = null;
+	
+	private volatile boolean exitThread = false;
+	
+	/**
+	 * Creates a new instance of the PacketQueueThread using the given client connection.
+	 * @param client A socket bound to the client to communicate with.
+	 */
+	public PacketQueueThread(Socket client)
 	{
 		super("PacketQueueThread");
+		socket = client;
+		
+		inbound = new LinkedList<Packet>();
+		outbound = new LinkedList<Packet>();
 	}
 	
 	public void run()
 	{
+		while(true)
+		{
+			try
+			{
+				synchronized(this)
+				{
+					
+				}
+			}
+			finally
+			{
+				// if exitThread is true, the parent thread has requested a soft termination of this thread
+				if(exitThread)
+					break;
+			}
+		}
 		
+		try
+		{
+			socket.close();
+		}
+		catch (IOException ex)
+		{
+			Trace.dprint("Unable to close client socket in PacketQueueThread.");
+		}
+	}
+	
+	/**
+	 * Enqueues a packet to be sent to this client.
+	 * @param packet Packet to send
+	 */
+	public synchronized void send(Packet packet)
+	{
+		outbound.add(packet);
+	}
+	
+	/**
+	 * Receives the next packet sent by this PacketQueueThread's client. This method returns null if there are no inbound packets.
+	 * @return Next packet sent by client or null if no such packet exists
+	 */
+	public synchronized Packet receive()
+	{
+		if(inbound.isEmpty())
+			return null;
+		else
+			return inbound.remove();
+	}
+	
+	/**
+	 * Returns true if this client has sent a packet
+	 * @return True if this client has sent a packet
+	 */
+	public synchronized boolean hasInbound()
+	{
+		return !inbound.isEmpty();
+	}
+	
+	/**
+	 * Sets a flag which will attempt to safely terminate this thread.
+	 */
+	public void stopThread()
+	{
+		exitThread = true;
 	}
 }
