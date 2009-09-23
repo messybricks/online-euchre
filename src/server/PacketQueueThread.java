09bdbaaed3be42c6797b5f6ea879816352442c54
sfAnimorphic <sfAnimorphic@284f36b8-9d99-11de-a36b-d11e950a0b04>
2009-09-23 20:50:03 +0000

    Added:  > EuchreServer now sends ping requests to clients at a specified interval and disconnects them if pongs are not received within a set amount of time. Notes:  > Clients cannot pong yet, so no connections will survive past three seconds.

--- src/server/PacketQueueThread.java
+++ src/server/PacketQueueThread.java
@@ -10,8 +10,32 @@ import utility.*;
  */
 public class PacketQueueThread extends NetworkThread
 {
+	private long lastPing = 0;
+	private long lastPong = 0;
+	
 	public PacketQueueThread(Socket client)
 	{
 		super(client);
+	}
+	
+	public long getLastPing()
+	{
+		return lastPing;
+	}
+	
+	public long getLastPong()
+	{
+		return lastPong;
+	}
+	
+	public void setPong()
+	{
+		lastPong = System.currentTimeMillis();
+	}
+	
+	public void ping()
+	{
+		send(new Packet(Opcode.Ping));
+		lastPing = System.currentTimeMillis();
 	}
 }
