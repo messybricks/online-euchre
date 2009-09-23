838488afe535e0ddf5ce3238663c7e0275bc56a3
sfAnimorphic <sfAnimorphic@284f36b8-9d99-11de-a36b-d11e950a0b04>
2009-09-23 17:28:03 +0000

    Modified:  > DEFAULT_PORT is now a constant in the server class Fixed:  > Configurable port is now correctly application argument 1

--- src/server/PacketQueueThread.java
+++ src/server/PacketQueueThread.java
@@ -2,8 +2,6 @@ package server;
 
 /**
  * This thread manages two queues; one for sending data over the network and one for receiving it.
- * @author rmspycha
- *
  */
 public class PacketQueueThread extends Thread
 {
