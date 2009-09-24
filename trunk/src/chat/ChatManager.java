5c29bc9883a030950730229d4878ce2caeacc9b7
KingJason1 <KingJason1@284f36b8-9d99-11de-a36b-d11e950a0b04>
2009-09-24 19:59:39 +0000

    Added documentation

--- trunk/src/chat/ChatManager.java
+++ trunk/src/chat/ChatManager.java
@@ -2,12 +2,27 @@ package chat;
 
 import utility.Packet;
 
+/**
+ * Class to manage chat messages.
+ * 
+ * @author Jason King
+ *
+ */
 class ChatManager
 {
+	/*
+	 * Default Constructor.
+	 */
     public ChatManager()
     {
     }
 
+    /*
+     * Send a chat object.
+     * 
+     * @param obj The chat object to send
+     * @return boolean True if it was sent succesfully, false otherwise
+     */
     public boolean send(ChatObject obj)
     {
         User dest = obj.getDest();
