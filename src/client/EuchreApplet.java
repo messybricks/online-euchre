1aae6b08e72f194caf4e742f340b3e9f429c80d5
KingJason1 <KingJason1@284f36b8-9d99-11de-a36b-d11e950a0b04>
2009-10-01 21:18:27 +0000

    got rid of JOptionPane to ask for username, becuase it was working before we added that...

--- src/client/EuchreApplet.java
+++ src/client/EuchreApplet.java
@@ -30,93 +30,88 @@ public class EuchreApplet extends JApplet implements ActionListener, KeyListener
 	 * Initializes the client and applet, calls helper methods setUpClient and setUpApplet
 	 */
 	public void init() {
-
+		
 		//set up the client
 		setUpClient();
-
+		
 		//set up the applet
 		setUpApplet();
-
+		
 
 	}
-	/**
-	 * uses JOptionPanes to ask user to join or host a game, then sets up a client and server (if applicable)
-	 */
+	 /**
+	  * uses JOptionPanes to ask user to join or host a game, then sets up a client and server (if applicable)
+	  */
 	private void setUpClient(){
 		//set up the client
 		//<<<<<<< .working
-		client = new EuchreNetClient("127.0.0.1", 36212, this);
+				client = new EuchreNetClient("127.0.0.1", 36212, this);
 		//=======
-		Object[] options = {"Host","Join" };
-		int n = JOptionPane.showOptionDialog(this,
-				"Would you like to host or join a game?",
-				"",
-				JOptionPane.YES_NO_CANCEL_OPTION,
-				JOptionPane.QUESTION_MESSAGE,
-				null,
-				options,
-				options[1]);
-		if(n==0)
-			madeserver=true;
-		String myAddress,serverNums;
-		String serverIP="", port="", username = "";
-		if(madeserver)
-		{
-			try
-			{		
-				String myName = InetAddress.getLocalHost().getHostName();
-				myAddress= InetAddress.getLocalHost().getHostAddress();
-				port =JOptionPane.showInputDialog("Your IP is " + myAddress +", your name is " + myName + "\n Choose Port:", "36212");
-				//TODO deal with improper port values
-				username =JOptionPane.showInputDialog("Enter username: ");
-				initializeUser(username);
-				String [] args= {port};
-				serverIP="127.0.0.1";
-				server = Runtime.getRuntime().exec("java server/EuchreServer",args);
-			}
-			catch(IOException e)
-			{
-				Trace.dprint("server failed to start: %s", e.getMessage());
-			}
-			//sleeps to give server time to initialize
-			try 
-			{
-				Thread.sleep(1000);
-			} 
-			catch (InterruptedException e) 
-			{
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-
-		}
-		else
-		{
-			//TODO deal with improper input
-			serverNums=JOptionPane.showInputDialog("Enter the Server IP:port","127.0.0.1:36212");
-			serverIP=serverNums.substring(0, serverNums.indexOf(':')).trim();
-			port=serverNums.substring(serverNums.indexOf(':')+1).trim();
-			Trace.dprint("the server ip is: %s and the port is %s", serverIP,port);
-			username =JOptionPane.showInputDialog("Enter username: ");
-			initializeUser(username);
-		}
-		client = new EuchreNetClient(serverIP, new Integer(port).intValue(), this);
+				Object[] options = {"Host","Join" };
+				int n = JOptionPane.showOptionDialog(this,
+							"Would you like to host or join a game?",
+							"",
+							JOptionPane.YES_NO_CANCEL_OPTION,
+							JOptionPane.QUESTION_MESSAGE,
+							null,
+							options,
+							options[1]);
+				if(n==0)
+					madeserver=true;
+				String myAddress,serverNums,serverIP="",port="";
+				if(madeserver)
+				{
+					try
+					{		
+						String myName = InetAddress.getLocalHost().getHostName();
+						myAddress= InetAddress.getLocalHost().getHostAddress();
+						port =JOptionPane.showInputDialog("Your IP is " + myAddress +", your name is " + myName + "\n Choose Port:", "36212");
+						//TODO deal with improper port values
+						String [] args= {port};
+						serverIP="127.0.0.1";
+						server = Runtime.getRuntime().exec("java server/EuchreServer",args);
+					}
+					catch(IOException e)
+					{
+						Trace.dprint("server failed to start: %s", e.getMessage());
+					}
+					//sleeps to give server time to initialize
+					try 
+					{
+						Thread.sleep(1000);
+					} 
+					catch (InterruptedException e) 
+					{
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+				}
+				else
+				{
+					//TODO deal with improper input
+					serverNums=JOptionPane.showInputDialog("Enter the Server IP:port","127.0.0.1:36212");
+					serverIP=serverNums.substring(0, serverNums.indexOf(':')).trim();
+					port=serverNums.substring(serverNums.indexOf(':')+1).trim();
+					Trace.dprint("the server ip is: %s and the port is %s", serverIP,port);
+				}
+				client = new EuchreNetClient(serverIP, new Integer(port).intValue(), this);
 		//>>>>>>> .merge-right.r48
-		//error messages
-		if(!client.isValid())
-		{
-			JOptionPane.showMessageDialog(this, "Unable to establish connection with server. Cannot continue.");
-			System.exit(-1);
-		}
-
-		if(!client.start())
-		{
-			JOptionPane.showMessageDialog(this, "Unable to start client core.");
-			System.exit(-1);
-		}
-
+				//error messages
+				if(!client.isValid())
+				{
+					JOptionPane.showMessageDialog(this, "Unable to establish connection with server. Cannot continue.");
+					System.exit(-1);
+				}
+				
+				if(!client.start())
+				{
+					JOptionPane.showMessageDialog(this, "Unable to start client core.");
+					System.exit(-1);
+				}
+				
 	}
-
+	
 	/**
 	 * initializes the layout and fields of the Applet
 	 */
@@ -129,9 +124,8 @@ public class EuchreApplet extends JApplet implements ActionListener, KeyListener
 		submit = new JButton("Submit");
 
 		//set up the text areas				
-		messageWindow.setText("Welcome to Online Euchre. \nEnter messages below.\n");
+		messageWindow.setText("Welcome to Online Euchre. \nPlease submit your username below.\n");
 		userWindow.setText("Users currently in chat:\n");
-		userWindow.append(currentUser.getUsername());
 		userWindow.setBackground(Color.LIGHT_GRAY);
 		//set up borders
 		messageWindow.setBorder(BorderFactory.createEtchedBorder());
@@ -172,25 +166,11 @@ public class EuchreApplet extends JApplet implements ActionListener, KeyListener
 		submit.addActionListener(this);
 		inputText.addActionListener(this);
 		inputText.addKeyListener(this);
-
-		//TODO: get this to work
-		inputText.setText("Enter your messages here.");
+		
+		//TODO: get this to work like the "Enter your messages here" thing
+		inputText.setText("Enter your username here.");
 		inputText.select(0, inputText.getText().length());
-
-
-	}
-
-
-	/**
-	 * initializes the user that is using this EuchreApplet
-	 * @param username the username this user inputs
-	 */
-	private void initializeUser(String username){
-		//Create the new user.
-		currentUser = new User(username);
-		//Authenticate client
-		//TODO: get this to work
-		client.authenticate(currentUser);
+		
 	}
 
 	/**
@@ -200,11 +180,25 @@ public class EuchreApplet extends JApplet implements ActionListener, KeyListener
 		String text = inputText.getText().trim();		
 		//if the message is not empty
 		if (!text.equals("")) {
-			sendMessage(inputText.getText(), currentUser);
-			//clear the text.
-			inputText.setText("");
-			//set focus back to the text back for easy message passing.
-			inputText.requestFocus();
+			//if the user has not signed in, then create a new user.
+			if(currentUser == null) {
+				//Create the new user.
+				currentUser = new User(inputText.getText());
+				//Update the user window.
+				userWindow.append(currentUser.getUsername());
+				//Set up the message text field.
+				//TODO Might need to remove if all screens only share one text area, rather then sharing the data in the area.
+				inputText.setText("Enter your messages here.");
+				inputText.select(0, inputText.getText().length());
+				client.authenticate(currentUser);
+			}
+			else {
+				sendMessage(inputText.getText(), currentUser);
+				//clear the text.
+				inputText.setText("");
+				//set focus back to the text back for easy message passing.
+				inputText.requestFocus();
+			}
 		}	
 	}
 
@@ -216,7 +210,7 @@ public class EuchreApplet extends JApplet implements ActionListener, KeyListener
 	 */
 	public void sendMessage(String text, User user) {
 		//add message to current users window.
-		//	messageWindow.append(user.getUsername() + ": " + text + "\n");
+	//	messageWindow.append(user.getUsername() + ": " + text + "\n");
 		//send out message to other users.
 		client.sendGlobalChatMessage(text);
 	}
@@ -243,7 +237,7 @@ public class EuchreApplet extends JApplet implements ActionListener, KeyListener
 			userWindow.append(user.getUsername());
 		}
 	}
-
+	
 	/**
 	 * closes client threads prior to the applet closing and sends a quit message to the server.
 	 */
@@ -290,13 +284,13 @@ public class EuchreApplet extends JApplet implements ActionListener, KeyListener
 	 */
 	@Override
 	public void keyPressed(KeyEvent e) {}
-
+	
 	/**
 	 * not implemented
 	 */
 	@Override
 	public void keyTyped(KeyEvent e) {}
-
+	
 
 
 }
