23f3e4c1f10a9c562208a08bfb6e4ed13cb31df8
messybricks <messybricks@284f36b8-9d99-11de-a36b-d11e950a0b04>
2009-10-23 19:09:05 +0000

    fixed it so the unit test works on barebones state machine

--- trunk/src/testing/EuchreEngineTest.java
+++ trunk/src/testing/EuchreEngineTest.java
@@ -5,14 +5,12 @@ import game.*;
 
 public class EuchreEngineTest extends TestCase {
 
+	private EuchreEngine e;
+	
 	public void testStart() 
 	{
-		
+		e = new EuchreEngine();
+		e.start();
+		assertTrue(e.getState() == EuchreEngine.END_OF_ROUND);
 	}
-	
-	public void testGetState()
-	{
-		
-	}
-
 }
