1f55906cb05d77490a5e71fa847a0b62eb4b7706
messybricks <messybricks@284f36b8-9d99-11de-a36b-d11e950a0b04>
2009-10-23 17:57:56 +0000

    started the EuchreEngine test

---
+++ src/testing/EuchreEngineTest.java
@@ -0,0 +1,18 @@
+package testing;
+
+import junit.framework.TestCase;
+import game.*;
+
+public class EuchreEngineTest extends TestCase {
+
+	public void testStart() 
+	{
+		
+	}
+	
+	public void testGetState()
+	{
+		
+	}
+
+}
