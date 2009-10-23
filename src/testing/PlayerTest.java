16e75c6699a68283d663577e2619d94d2298c646
roaikgid <roaikgid@284f36b8-9d99-11de-a36b-d11e950a0b04>
2009-10-23 17:16:37 +0000

    >created the skeleton for the Player Class Junit testing

---
+++ src/testing/PlayerTest.java
@@ -0,0 +1,58 @@
+package testing;
+
+import chat.User;
+import game.*;
+import junit.framework.TestCase;
+
+public class PlayerTest extends TestCase 
+{
+	
+	private Player player;
+
+	public void testPlayerUser() 
+	{
+		player = new Player(new User("test"));
+		
+		fail("Not yet implemented");
+	}
+
+	public void testPlayerString() 
+	{
+		fail("Not yet implemented");
+	}
+
+	public void testPickupCard() 
+	{
+		fail("Not yet implemented");
+	}
+
+	public void testPlayCard() 
+	{
+		fail("Not yet implemented");
+	}
+
+	public void testMoveCard() 
+	{
+		fail("Not yet implemented");
+	}
+
+	public void testSwapCards() 
+	{
+		fail("Not yet implemented");
+	}
+
+	public void testGetUsername() 
+	{
+		fail("Not yet implemented");
+	}
+
+	public void testToString() 
+	{
+		fail("Not yet implemented");
+	}
+	
+	public void setUp()
+	{
+	}
+
+}
