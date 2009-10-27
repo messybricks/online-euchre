40b7025ea8b8abd0a2da265306f7098f07c44a9e
roaikgid <roaikgid@284f36b8-9d99-11de-a36b-d11e950a0b04>
2009-10-27 17:51:32 +0000

    >added carddistributortest class >started to add a test for shuffle in cardTest, should have a 1 in 24factorial chance of failing when done

---
+++ src/testing/CardDistributorTest.java
@@ -0,0 +1,25 @@
+package testing;
+import game.*;
+
+import junit.framework.TestCase;
+
+public class CardDistributorTest extends TestCase {
+
+	
+	public void testDealRound() {
+		fail("Not yet implemented");
+	}
+
+	public void testFlipTrump() {
+		fail("Not yet implemented");
+	}
+
+	public void testNextRound() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetPlayerOrder() {
+		fail("Not yet implemented");
+	}
+
+}
