a5e908480ce0e6b061421d5e9dd633f126036a91
messybricks <messybricks@284f36b8-9d99-11de-a36b-d11e950a0b04>
2009-12-02 18:14:00 +0000

    got rid of flipDownTrump

--- src/game/EuchreEngine.java
+++ src/game/EuchreEngine.java
@@ -105,7 +105,7 @@ public class EuchreEngine
 		{
 			//send an opcode to each player to show that the trump card is flipped down
 			for(int i = 0; i<4; i++)
-				cardDistributor.getPlayerOrder()[i].sendOpcode(Opcode.flipDownTrump);
+				cardDistributor.getPlayerOrder()[i].sendData(Opcode.displayCard, Card.nullCard());
 		}
 
 		//send opcode to player to request a bid
